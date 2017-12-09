89ed8804e7ee9ce0f944129e1dae008c73f69824
JinHui <1004260403@qq.com>
2017-12-09 12:48:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/appcache/User.java
@@ -0,0 +1,12 @@
+package com.example.jh.appcache;
+
+import java.io.Serializable;
+
+/**
+ * Created by Fussen on 2017/12/6.
+ */
+
+public class User implements Serializable{
+    public String name;
+    public String age;
+}
